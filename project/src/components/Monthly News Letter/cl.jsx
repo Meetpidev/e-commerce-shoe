4e72f7f8e1298129c12d326467d0611a11cb28d3
kinshuk bageja <kinshukbageja@gmail.com>
2024-06-20 22:57:19 +0530

    corousel updared

--- project/src/components/Monthly News Letter/cl.jsx
+++ project/src/components/Monthly News Letter/cl.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import './Carousel.css'; 
 import nike from '../../assets/Top_Brands/NikeLogo.png';
 import gucci from "../../assets/Top_Brands/gucci.png";
 import lv from "../../assets/Top_Brands/lv.png";
 import adidas from "../../assets/Top_Brands/adidas.png";
 import skechers from "../../assets/Top_Brands/Skechers-logo.png";
-import './cl.css/'; 
 
 const images = [
   nike,
@@ -17,36 +17,16 @@ const images = [
   lv,
   skechers,
   adidas,
-  
 ];
 
 const Cl = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const getDisplayedImages = () => {
-    const start = currentIndex;
-    const end = (currentIndex + 4) % images.length;
-
-    if (end > start) {
-      return images.slice(start, end);
-    } else {
-      return [...images.slice(start), ...images.slice(0, end)];
-    }
-  };
-
   return (
     <div className="carousel2">
-      {getDisplayedImages().map((image, index) => (
-        <img key={index} src={image} alt={`Slide ${index}`} className="carousel-image" />
-      ))}
+      <div className="carousel-inner">
+        {images.map((image, index) => (
+          <img key={index} src={image} alt={`Slide ${index}`} className="carousel-image" />
+        ))}
+      </div>
     </div>
   );
 };
