f5eb20348215083b150e669e550b499b06168489
Meet <91799@Meet>
2024-06-21 19:56:50 +0530

    conflicts resolved

--- project/src/components/Monthly News Letter/cl.jsx
+++ project/src/components/Monthly News Letter/cl.jsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from 'react';
 import './Carousel.css'; 
 import nike from '../../assets/Top_Brands/NikeLogo.png';
 import gucci from "../../assets/Top_Brands/gucci.png";
@@ -17,16 +17,36 @@ const images = [
   lv,
   skechers,
   adidas,
+  
 ];
 
 const Cl = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const getDisplayedImages = () => {
+    const start = currentIndex;
+    const end = (currentIndex + 4) % images.length;
+
+    if (end > start) {
+      return images.slice(start, end);
+    } else {
+      return [...images.slice(start), ...images.slice(0, end)];
+    }
+  };
+
   return (
     <div className="carousel2">
-      <div className="carousel-inner">
-        {images.map((image, index) => (
-          <img key={index} src={image} alt={`Slide ${index}`} className="carousel-image" />
-        ))}
-      </div>
+      {getDisplayedImages().map((image, index) => (
+        <img key={index} src={image} alt={`Slide ${index}`} className="carousel-image" />
+      ))}
     </div>
   );
 };
